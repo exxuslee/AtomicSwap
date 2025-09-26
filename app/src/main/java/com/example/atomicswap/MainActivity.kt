d093225f6dcbdc3301d8da3644456e1adb08e116
Oleksij Romanenko <exxuslee@gmail.com>
2025-09-27 01:21:44 +0300

    Exit on double press back

--- app/src/main/java/com/example/atomicswap/MainActivity.kt
+++ app/src/main/java/com/example/atomicswap/MainActivity.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.compose.BackHandler
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import com.example.atomicswap.core.ui.theme.AppTheme
 import com.example.atomicswap.domain.usecases.ThemeController
+import com.example.atomicswap.utils.rememberDoubleBackPressHandler
 import org.koin.compose.koinInject
 
 class MainActivity : ComponentActivity() {
@@ -18,6 +20,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             val themeController: ThemeController = koinInject()
             val isDark by themeController.isDark.collectAsState()
+            val doubleBackPressHandler = rememberDoubleBackPressHandler(this@MainActivity)
+
+            BackHandler {
+                doubleBackPressHandler.handleBackPress()
+            }
 
             AppTheme(isDark) {
                 MainContent()
